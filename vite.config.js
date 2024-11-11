e0c534142d07ed0a38c6e08bcf02525f21263e39
badrinarayanms <badrinarayan.moola@gmail.com>
2024-11-11 12:56:34 +0530

    check05

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
-  build: {
-    sourcemap: true, // Ensure sourcemaps are enabled for production build
-  },
-  esbuild: {
-    sourcemap: true, // Force source maps in esbuild
-  },
-});
+  assetsInclude: ['**/*.glb'], // Add this line to include .glb files
+})
