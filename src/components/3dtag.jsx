dacb7489484108819a5417bd16f65fb40c38c57c
badrinarayanms <badrinarayan.moola@gmail.com>
2024-11-11 09:18:52 +0530

    path updated01

--- src/components/3dtag.jsx
+++ src/components/3dtag.jsx
@@ -5,7 +5,7 @@ import { Canvas, extend, useThree, useFrame } from '@react-three/fiber'
 import { useGLTF, useTexture, Environment, Lightformer } from '@react-three/drei'
 import { BallCollider, CuboidCollider, Physics, RigidBody, useRopeJoint, useSphericalJoint } from '@react-three/rapier'
 import { MeshLineGeometry, MeshLineMaterial } from 'meshline'
-import banddd from 'public/assets/band.jpg' 
+import banddd from '/public/assets/band.jpg' 
 import basetag from '/public/assets/scene.glb' 
 import Titlee from './Titlee'
 // const basetag = 'public/assets/scene.glb';
